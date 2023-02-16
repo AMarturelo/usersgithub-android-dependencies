4deee4d29599d4b035e35e06f761db22c7531c60
Alberto <amarturelo@gmail.com>
2023-02-16 16:01:45 -0300

    add version app

---
+++ buildSrc/src/main/kotlin/dependencies/dependencies.gradle.kt
@@ -0,0 +1,6 @@
+object Configuration{
+    const val projectName = "usersgithub-android-core"
+    const val groupName = "com.amarturelo.usersgithub.android"
+    const val versionName = "1.0.0"
+    const val artifactId = "core"
+}
