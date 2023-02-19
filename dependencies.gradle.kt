e410e2dab6a93690d49a3ad5edf32c327e851b3d
Alberto <amarturelo@gmail.com>
2023-02-19 15:54:27 -0300

    add dependencies

--- dependencies.gradle.kt
+++ dependencies.gradle.kt
@@ -1,7 +1,23 @@
-object VersionApp{
+object VersionApp {
     //Sdk
     const val compileSdkVersion = 31
     const val targetSdkVersion = 31
     const val minSdkVersion = 23
-    const val minSdkVersion2 = 23
+
+    //Kotlin
+    const val kotlinVersion = "1.5.31"
+    const val navComponentVersion = "2.4.1"
+    const val androidGradlePluginVersion = "7.0.3"
+
+    const val jacocoVersion = "7.0.3"
+}
+
+object ClasspathDependencies {
+    const val kotlinGradlePlugin =
+        "org.jetbrains.kotlin:kotlin-gradle-plugin:${VersionApp.kotlinVersion}"
+    const val androidGradlePlugin =
+        "com.android.tools.build:gradle:${VersionApp.androidGradlePluginVersion}"
+    const val navComponentSafeVarargs =
+        "androidx.navigation:navigation-safe-args-gradle-plugin:${VersionApp.navComponentVersion}"
+    const val jacocoPuglin = "org.jacoco:org.jacoco.core:${VersionApp.jacocoVersion}"
 }
